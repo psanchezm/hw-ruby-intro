e919c68c8dd6f38a461033fd49a5b9adafff35cd
Paula A Sanchez <paulas12@gmail.com>
2016-06-20 03:35:37 +0000

    Ex 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,37 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  x = 0
+  arr.each { |a| x+=a }
+  return x
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort!
+  return arr.length == 0 ? 0 : arr.length == 1 ? arr.pop : arr.pop + arr.pop
 end
-
+#sum_to_n? [1,2,3,4,5], 5                          
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  x = false
+  arr.length > 1 ?
+    ( arr.each do |a| 
+      x = arr.select { |c| c != a }.select { |b| a + b == n }.length > 0 
+      x ? break : false
+    end
+    )
+  : x = false
+  return x
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  #/[^aeiou]/.match(s.downcase[0]) != nil #no sirve para los nonletter characters
+  /[b-df-hj-np-tv-z]/.match(s.downcase[0]) != nil
 end
 
 def binary_multiple_of_4? s
